d816447fc4ca35fc4f2b33b5e82ddf6bc8e5c08d
minsu.medison@gmail.com <minsu.medison@gmail.com>
2024-09-02 22:55:21 +0900

    View 작성을 독립적으로 실행하기 위해 CountdownView라는 View 추가

---
+++ VersusTimer/CountdownView.swift
@@ -0,0 +1,24 @@
+//
+//  CountdownView.swift
+//  VersusTimer
+//
+//  Created by Minsu Han on 9/2/24.
+//
+
+import SwiftUI
+
+struct CountdownView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CountdownView()
+}
