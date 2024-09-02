a43211b59d407fbf1e6e4e972e5850d295312d55
minsu.medison@gmail.com <minsu.medison@gmail.com>
2024-09-02 22:45:12 +0900

    Project inititialize

---
+++ VersusTimer/VersusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersusTimerApp.swift
+//  VersusTimer
+//
+//  Created by Minsu Han on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct VersusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
